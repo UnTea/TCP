f8b3379f69cef8112c7cf7830bf02d52cad8f7e2
Egor Aksenov <weqqu2014@yandex.ru>
2022-10-26 16:42:28 +0300

    add server

--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,25 @@
 #include <boost/asio.hpp>
+#include <iostream>
+
+using boost::asio::ip::tcp;
 
 int main() {
-    return 0;
+    try {
+        boost::asio::io_context io_context;
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 42069));
+
+        while (true) {
+            std::cout << "Accepting connection on port 42069\n";
+            tcp::socket socket(io_context);
+            acceptor.accept(socket);
+
+            std::cout << "Client connected! Sending message!\n";
+            std::string hello_message = "Hello from server!\n";
+            boost::system::error_code error;
+
+            boost::asio::write(socket, boost::asio::buffer(hello_message), error);
+        }
+    } catch (std::exception& exception) {
+        std::cerr << exception.what() << std::endl;
+    }
 }
