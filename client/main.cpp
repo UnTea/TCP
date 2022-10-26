bb112af8ff2f2f6af8e54c1a47fae75970504707
Egor Aksenov <weqqu2014@yandex.ru>
2022-10-26 17:06:54 +0300

    add client

--- client/main.cpp
+++ client/main.cpp
@@ -1,5 +1,34 @@
+#include <array>
 #include <boost/asio.hpp>
+#include <iostream>
 
-int main() {
-    return 0;
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[]) {
+    try {
+        boost::asio::io_context io_context;
+        tcp::resolver resolver{io_context};
+        auto endpoint = resolver.resolve("127.0.0.1", "42069");
+        tcp::socket socket{io_context};
+        boost::asio::connect(socket, endpoint);
+
+        while (true) {
+            // Listen for messages
+
+            std::array<char, 128> buffer{};
+            boost::system::error_code error;
+            size_t length = socket.read_some(boost::asio::buffer(buffer), error);
+
+            if (error == boost::asio::error::eof) {
+                // Clean connection cut off
+                break;
+            } else if (error) {
+                throw boost::system::system_error(error);
+            }
+
+            std::cout.write(buffer.data(), length);
+        }
+    } catch (std::exception& exception) {
+        std::cerr << exception.what() << std::endl;
+    }
 }
